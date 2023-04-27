72fd278da16c3952eaa39667edb4dc59e78b20dd
Tendonin46 <ali.ichelmman@hotmail.com>
2023-04-27 16:09:13 +0200

    EX_2

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -2,11 +2,17 @@ package ilu2;
 
 public class Welcome {
 	public static String welcome(String prenom) {
-		StringBuilder chaine = new StringBuilder();
-	    chaine.append("Hello, ");
-	    chaine.append(prenom.substring(0, 1).toUpperCase());
-	    chaine.append(prenom.substring(1));
-	    return chaine.toString();
+		if (prenom == null || prenom.trim().isEmpty()) {
+	        return "Hello, my friend";
+	    } 
+		else {
+			StringBuilder chaine = new StringBuilder("Hello, ");
+		    chaine.append(prenom.substring(0, 1).toUpperCase());
+		    chaine.append(prenom.substring(1));
+		    return chaine.toString();
+	    }
+	     
+
 	}
 
 	public static void main (String[] args) {
--- tests/ilu2/WelcomeTest.java
+++ tests/ilu2/WelcomeTest.java
@@ -10,13 +10,43 @@ import org.junit.jupiter.api.Test;
 
 public class WelcomeTest {
 	
+	private Welcome M;
+	
+	@BeforeEach
+	public void initialiserSituation() {
+		Welcome M=new Welcome();
+	}
+	
+	
 	 @Test
 	public void test_hello() {
-		 Welcome M=new Welcome();
 		String prenom = "bob";
         String attendu = "Hello, Bob";
         String actuel = M.welcome(prenom);
         assertEquals(attendu, actuel);
-		
 	}
+	@Test
+	public void test_hello_null() {
+		String prenom = null;
+        String attendu = "Hello, my friend";
+        String actuel = M.welcome(prenom);
+        assertEquals(attendu, actuel);
+	}
+	
+	@Test
+	public void test_hello_chaine_vide() {
+		String prenom = "";
+        String attendu = "Hello, my friend";
+        String actuel = M.welcome(prenom);
+        assertEquals(attendu, actuel);
+	}
+	
+	@Test
+	public void test_hello_blanche() {
+		String prenom ="      ";
+        String attendu = "Hello, my friend";
+        String actuel = M.welcome(prenom);
+        assertEquals(attendu, actuel);
+	}
+	
 }
